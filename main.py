87f621450a4959b8f12b22b18708cbf607f2c565
Jimly23 <jimlyasidqi692@gmail.com>
2023-10-10 22:37:29 +0700

    Mengubah file main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-hasil = {
-    'nama':'Jimly assidqi hardiansyah',
-    'umur':18,
-    'alamat':'Buniwah'
-}
+mahasiswa = [
+    {
+        'nama':'Jimly assidqi hardiansyah',
+        'umur':18,
+        'alamat':'Buniwah'
+    },
+    {
+        'nama':'Muhammad arya dwi',
+        'umur':18,
+        'alamat':'Kalilumpang'
+    }
+]
